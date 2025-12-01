d1604ccdd1541022dd5bdff8403c13188d1725cd
hungwei <hungwei@LAPTOP-6VIAIBS1.localdomain>
2025-12-01 22:59:58 +0800

    added two files

---
+++ utils.py
@@ -0,0 +1,41 @@
+[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/1KM97C_X)
+[![Open in Codespaces](https://classroom.github.com/assets/launch-codespace-2972f46106e565e64193e422d61a12cf1da4916b45550586e14ef0a7c637dd04.svg)](https://classroom.github.com/open-in-codespaces?assignment_repo_id=21307722)
+# Computational Physics Final Project 
+
+## Topic
+Monte Carlo computation of spin-1/2 Heisenberg model
+
+## Introduction and Expected Achievement
+Spin system provides a model for describing various physical system, which is crucial in manybody physics. It allows us to study the solid states physics and quantum chemistry. In particular, the Heisenberg model [1] is a fundamental paradigm for the spin-spin interaction in solid. It can be used to explain the magnetic properties of solid. However, solving the quantum manybody system is computationally challenging due to the large Hilbert space that grows with the system size. Monte Carlo method [2-3] gives a alternative way to compute integration in high-dimensional integration with lower computational complexity, which is useful in quantum mechanics calculations. By using the Stochastic Series Expansion (SSE) [4], we could sample the large Hilbert space with probability that follows the Boltzmann distribution, allowing us to calculate the expectation values of certain physical parameters. In this work, we apply the quantum Monte Carlo method to slove the physical parameters of the Heisenberg model. We study the magnetic susceptibility and the correlation length, and investigate the finite-size scaling of the Heisenberg model. These are essential for us to understand the physics behind Heisenberg model.
+  
+## Name and Student ID
+
+林韋辰 B11202038
+劉翃瑋 B11202032
+
+## Method
+
+Brief summary of your method
+
+## Video link (youtube)
+
+You are required to record a 3-min preview video to be available on 12/9
+
+## Planning and Timeline
+
+| Week   | Task |
+|--------|------|
+| 1 & 2  |  Literature review   |
+| 3   |  Code implementation   |
+| 4  |   Generated results   |
+| 6  |   Data analysis   |
+
+## User Guide
+
+Simple description of how to run your simulation and analysis programs.
+
+## References
+[1] Katsura, S. (1962). Statistical mechanics of the anisotropic linear Heisenberg model. Physical Review, 127(5), 1508.  
+[2] Chen, Y. C., Chen, H. H., & Lee, F. (1991). Quantum Monte Carlo study of the spin-1/2 Heisenberg model. Physical Review B, 43(13), 11082.  
+[3] Makivić, M. S., & Ding, H. Q. (1991). Two-dimensional spin-1/2 Heisenberg antiferromagnet: A quantum Monte Carlo study. Physical Review B, 43(4), 3562.  
+[4] Sandvik, A. W. (2010, November). Computational studies of quantum spin systems. In AIP Conference Proceedings (Vol. 1297, No. 1, pp. 135-338). American Institute of Physics.
